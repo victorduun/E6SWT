e2c57fccf44da2b917091305d7653f42770f5e50
victorduun <43340964+victorduun@users.noreply.github.com>
2020-10-08 23:11:29 +0200

    Add StationControl to project, with a few changes from handout where it injects the display, door and usbcharger dependencies through constructor. Also added a test which currently fails always for stationcontrol. Added NSubstitute nuget package to Test project

---
+++ Handin2/Ladeskab/Ladeskab.Test/StationControlTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ladeskab.Interfaces;
+using Ladeskab.USB;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Ladeskab.Test
+{
+    public class StationControlTest
+    {
+
+        private StationControl _uut;
+        private IUsbCharger _usbController;
+        private IDisplay _display;
+        private IDoor _door;
+
+        [SetUp]
+        public void Setup()
+        {
+            _usbController = Substitute.For<IUsbCharger>();
+            _display = Substitute.For<IDisplay>();
+            _door = Substitute.For<IDoor>();
+
+            _uut = new StationControl(_usbController, _door, _display);
+        }
+
+        [Test]
+        public void StationControl_Overcurrent_DisplayValueIsCorrect()
+        {
+            Assert.Fail("TODO: Fix this test; connect a device, cause an overcurrent event and assert that the display shows an error message");
+
+            //Setup usb controlller return values and invoke events (cause an overcurrent event)
+            //Max 500 mA
+            int overcurrentValue = 1000;
+            _usbController.CurrentValue.Returns(overcurrentValue);
+            var currentEventArgs = new CurrentEventArgs()
+            {
+                Current = overcurrentValue,
+            };
+            _usbController.CurrentValueEvent += Raise.Event<EventHandler<CurrentEventArgs>>(this, currentEventArgs);
+
+            //Assert that display shows correct error
+            _display.Received().ShowOvercurrentError();
+
+
+        }
+
+
+
+
+    }
+}
---
+++ Handin2/Ladeskab/Ladeskab/StationControl.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ladeskab.Interfaces;
+using Ladeskab.USB;
+
+namespace Ladeskab
+{
+    public class StationControl
+    {
+        // Enum med tilstande ("states") svarende til tilstandsdiagrammet for klassen
+        private enum LadeskabState
+        {
+            Available,
+            Locked,
+            DoorOpen
+        };
+
+        private LadeskabState _state;
+        private readonly IUsbCharger _charger;
+        private readonly IDoor _door;
+        private readonly IDisplay _display;
+        private int _oldId;
+
+        private string logFile = "logfile.txt"; // Navnet på systemets log-fil
+
+      
+        public StationControl(IUsbCharger usbCharger, IDoor door, IDisplay display)
+        {
+            _charger = usbCharger;
+            _door = door;
+            _display = display;
+        }
+        // Eksempel på event handler for eventet "RFID Detected" fra tilstandsdiagrammet for klassen
+        private void RfidDetected(int id)
+        {
+            switch (_state)
+            {
+                case LadeskabState.Available:
+                    // Check for ladeforbindelse
+                    if (_charger.Connected)
+                    {
+                        _door.Lock();
+                        _charger.StartCharge();
+                        _oldId = id;
+                        using (var writer = File.AppendText(logFile))
+                        {
+                            writer.WriteLine(DateTime.Now + ": Skab låst med RFID: {0}", id);
+                        }
+
+                        _display.ShowChargingLockerOccupied();
+                        _state = LadeskabState.Locked;
+                    }
+                    else
+                    {
+                        _display.ShowConnectionError();
+                    }
+
+                    break;
+
+                case LadeskabState.DoorOpen:
+                    // Ignore
+                    break;
+
+                case LadeskabState.Locked:
+                    // Check for correct ID
+                    if (id == _oldId)
+                    {
+                        _charger.StopCharge();
+                        _door.Unlock();
+                        using (var writer = File.AppendText(logFile))
+                        {
+                            writer.WriteLine(DateTime.Now + ": Skab låst op med RFID: {0}", id);
+                        }
+
+                        _display.ShowRemoveDevice();
+                        _state = LadeskabState.Available;
+                    }
+                    else
+                    {
+                        _display.ShowRfidError();
+                    }
+
+                    break;
+            }
+        }
+
+        // Her mangler de andre trigger handlere
+    }
+}
