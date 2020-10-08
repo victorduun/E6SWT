89b66820008b6b12c98e73056703b6ccc4216b1b
victorduun <43340964+victorduun@users.noreply.github.com>
2020-10-08 15:32:08 +0200

    Added ChargeControl file with dummy implementations of ICHargecontrol methods

---
+++ Handin2/Ladeskab/Ladeskab/ChargeControl.cs
@@ -0,0 +1,32 @@
+﻿using Ladeskab.Interfaces;
+using Ladeskab.USB;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ladeskab
+{
+    class ChargeControl : IChargeControl
+    {
+        IUsbCharger _usbCharger;
+        public void ConnectDevice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsConnected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartCharge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopCharge()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
