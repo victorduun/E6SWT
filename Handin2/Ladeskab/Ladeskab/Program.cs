5986827ac28bb13c461c30c9663c757e3a4ae9b6
victorduun <43340964+victorduun@users.noreply.github.com>
2020-10-08 12:31:46 +0200

    Move handout files to new folder, start a new VS solution with console application + test project with added nuget packages for jenkins continous integration

---
+++ Handin2/Ladeskab/Ladeskab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ladeskab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
